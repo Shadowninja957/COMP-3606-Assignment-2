2410469240a5e8b2376a9ce73c5458f67de5fc79
Dharian Malchan <dharianmalchan@gmail.com>
2021-11-13 11:31:56 -0400

    Updated TopLevelActivity and Added Fragment Classes

--- app/src/main/java/com/example/productmanagement/MainActivity.java
+++ app/src/main/java/com/example/productmanagement/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.productmanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +15,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> listView, View itemView, int position, long id) {
+                if (position == 0){
+                    Intent intent = new Intent(MainActivity.this,
+                            OrderingStocksActivity.class);
+                    startActivity(intent);
+                }
+                else if (position == 1){
+                    Intent intent = new Intent(MainActivity.this,
+                            ReceivingStocksActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+
+        ListView listView = (ListView) findViewById(R.id.list_options);
+        listView.setOnItemClickListener(itemClickListener);
     }
 }
---
+++ app/src/main/java/com/example/productmanagement/ReceivingStocksActivity.java
@@ -0,0 +1,14 @@
+package com.example.productmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReceivingStocksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receiving_stocks);
+    }
+}
